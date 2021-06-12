5ad3a5de5c12b06ef7fe297755a9740c74506d16
Yashwanth Sridharan <karthikyashwanth2009@gmail.com>
2021-06-13 00:48:08 +0530

    Updated Logout on token expire

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,30 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
 const Navbar = ({ theme, setTheme }) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const location = useLocation();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   console.log(user);
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    history.push("/");
+    setUser(null);
+  };
   useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date.getTime()) logout();
+    }
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
 
   const modeChangeHandler = () => {
     setTheme(!theme);
-  };
-
-  const logout = () => {
-    dispatch({ type: "LOGOUT" });
-    history.push("/");
-    setUser(null);
   };
 
   return (
